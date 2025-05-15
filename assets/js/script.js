10ba4ca2bc54e66172ccc154183694d558189d7a
Anderson Moreira <andersonmoreira507@gmail.com>
2025-05-14 22:09:53 -0300

    chore: removing unnecessary code

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,56 +35,6 @@ function menuMobile() {
 
 menuMobile();
 
-function sobreMim() {
-
-  const divExperiencia = document.querySelectorAll(".experience_content div");
-  const liExperiencia = document.querySelectorAll(".experience_content ul li");
-  const divEducation = document.querySelectorAll(".education_content div");
-  const liEducation = document.querySelectorAll(".education_content ul li");
-
-    divExperiencia[0].classList.add('ativo')
-    divEducation[0].classList.add('ativo')
-    liEducation[0].classList.add('ativo')
-    liExperiencia[0].classList.add('ativo')
-
-
-  function slideShow(index) {
-    divExperiencia.forEach((div) => {
-      div.classList.remove("ativo");
-    });
-    liExperiencia.forEach((botao) => {
-      botao.classList.remove("ativo");
-    });
-    divExperiencia[index].classList.add("ativo");
-    liExperiencia[index].classList.add("ativo");
-  }
-
-  function slideShow2(index) {
-    divEducation.forEach((div) => {
-      div.classList.remove("ativo");
-    });
-    liEducation.forEach((botao) => {
-      botao.classList.remove("ativo");
-    });
-    divEducation[index].classList.add("ativo");
-    liEducation[index].classList.add("ativo");
-  }
-
-  liExperiencia.forEach((event, index) => {
-    event.addEventListener("click", () => {
-      slideShow(index);
-    });
-  });
-
-  liEducation.forEach((event, index) => {
-    event.addEventListener("click", () => {
-      slideShow2(index);
-    });
-  });
-}
-
-sobreMim();
-
 // Navegação suave ao clicar nos links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
